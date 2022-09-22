3db438897b11f8e3e5f7999d75f9258d8ba2824f
Z-SooS <zoli.soos98@gmail.com>
2022-09-22 10:39:15 +0200

    Deleted unnecessary enums

--- AskMateREST/src/main/java/practice/askmaterest/utility/utilenums/GetQueryFor.java
+++
@@ -1,6 +0,0 @@
-package practice.askmaterest.utility.utilenums;
-
-public enum GetQueryFor {
-    USER,
-    QUESTION
-}
--- AskMateREST/src/main/java/practice/askmaterest/utility/utilenums/TagOperation.java
+++
@@ -1,6 +0,0 @@
-package practice.askmaterest.utility.utilenums;
-
-public enum TagOperation {
-    TAG,
-    UNTAG
-}
