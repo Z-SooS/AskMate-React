4c15e26b32c3d926601bbc1961b96eb74454ef3f
Z-SooS <zoli.soos98@gmail.com>
2022-09-23 11:57:32 +0200

    Services for created entities

---
+++ AskMateREST/src/main/java/practice/askmaterest/services/WebUserService.java
@@ -0,0 +1,9 @@
+package practice.askmaterest.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class WebUserService {
+}
