ba4abbbce08a2667adfa5bd66347f1f845feb2c9
Z-SooS <zoli.soos98@gmail.com>
2022-10-10 13:02:27 +0200

    Add 'save' method to WebUserService

--- AskMateREST/src/main/java/practice/askmaterest/services/WebUserService.java
+++ AskMateREST/src/main/java/practice/askmaterest/services/WebUserService.java
@@ -2,8 +2,16 @@ package practice.askmaterest.services;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import practice.askmaterest.model.WebUser;
+import practice.askmaterest.services.daos.WebUserRepo;
 
 @Service
 @AllArgsConstructor
 public class WebUserService {
+    private final WebUserRepo webUserRepo;
+
+    public void AddWebUser(WebUser user)
+    {
+        webUserRepo.save(user);
+    }
 }
