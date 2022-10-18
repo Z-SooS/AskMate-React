ecc215a6b218cf074ad9d05cd25e96f3bda2f6db
Z-SooS <zoli.soos98@gmail.com>
2022-10-18 10:50:09 +0200

    Add empty tag service

---
+++ AskMateREST/src/main/java/practice/askmaterest/services/TagService.java
@@ -0,0 +1,10 @@
+package practice.askmaterest.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TagService {
+
+}
