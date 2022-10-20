4a678794c7fba3e994585b05971c58e6ce029a24
Z-SooS <zoli.soos98@gmail.com>
2022-10-20 12:27:20 +0200

    Implement method for checking login credentials

--- AskMateREST/src/main/java/practice/askmaterest/security/CookieAgent.java
+++ AskMateREST/src/main/java/practice/askmaterest/security/CookieAgent.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import static practice.askmaterest.security.JwtUtil.base64Encode;
+import static practice.askmaterest.security.EncoderAgent.base64Encode;
 
 @Component
 public class CookieAgent {
--- AskMateREST/src/main/java/practice/askmaterest/services/WebUserService.java
+++ AskMateREST/src/main/java/practice/askmaterest/services/WebUserService.java
@@ -3,15 +3,28 @@ package practice.askmaterest.services;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import practice.askmaterest.model.WebUser;
+import practice.askmaterest.model.securityModel.LoginDetails;
+import practice.askmaterest.security.PasswordAgent;
 import practice.askmaterest.services.daos.WebUserRepo;
 
 @Service
 @AllArgsConstructor
 public class WebUserService {
     private final WebUserRepo webUserRepo;
+    private final PasswordAgent passwordAgent;
 
-    public void AddWebUser(WebUser user)
-    {
+    public boolean isCorrectLogin(LoginDetails loginDetails) {
+        var user = webUserRepo.findById(loginDetails.getUsername());
+        if (user.isEmpty()) return false;
+        boolean isCorrectPassword = passwordAgent.matchesPassword(loginDetails.getPassword(), user.get().getPassword());
+        if (isCorrectPassword) {
+            loginDetails.setEmail(user.get().getEmail());
+            return true;
+        }
+        return false;
+    }
+
+    public void AddWebUser(WebUser user) {
         webUserRepo.save(user);
     }
 }
