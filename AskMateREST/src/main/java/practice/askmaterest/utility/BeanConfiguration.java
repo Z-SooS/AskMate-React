b2687a66aaa16b2421473a153adde0b9d0b8cc08
Z-SooS <zoli.soos98@gmail.com>
2022-10-20 12:01:53 +0200

    Bean returns generic password encoder type

--- AskMateREST/src/main/java/practice/askmaterest/utility/BeanConfiguration.java
+++ AskMateREST/src/main/java/practice/askmaterest/utility/BeanConfiguration.java
@@ -3,11 +3,12 @@ package practice.askmaterest.utility;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeanConfiguration {
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoderFactory()
+    public PasswordEncoder bCryptPasswordEncoderFactory()
     {
         return new BCryptPasswordEncoder();
     }
