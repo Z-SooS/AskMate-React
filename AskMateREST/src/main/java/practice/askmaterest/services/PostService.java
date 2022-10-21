005ae32314b697caa10010f6ba2b691f66c7f552
Z-SooS <zoli.soos98@gmail.com>
2022-10-21 13:43:38 +0200

    Implement method to get paginated posts

--- AskMateREST/src/main/java/practice/askmaterest/services/PostService.java
+++ AskMateREST/src/main/java/practice/askmaterest/services/PostService.java
@@ -1,9 +1,23 @@
 package practice.askmaterest.services;
 
-import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import practice.askmaterest.model.Post;
+import practice.askmaterest.services.daos.PostRepo;
+
+import java.util.List;
 
 @Service
-@AllArgsConstructor
 public class PostService {
+    private final PostRepo postRepo;
+
+    private final int numberOfPostsPerPage = 6;
+
+    public PostService(PostRepo postRepo) {
+        this.postRepo = postRepo;
+    }
+
+    public List<Post> getPostsForPage(int page) {
+        return postRepo.findAll(PageRequest.of(page,numberOfPostsPerPage)).getContent();
+    }
 }
