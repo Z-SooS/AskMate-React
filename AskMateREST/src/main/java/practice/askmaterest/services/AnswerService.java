7e8dfbd4bda69c615de0e37049e140c9451441d5
Z-SooS <zoli.soos98@gmail.com>
2022-10-21 08:58:31 +0200

    Add pagination method to AnswerRepo interface, implement pagination methods in AnswerService

--- AskMateREST/src/main/java/practice/askmaterest/services/AnswerService.java
+++ AskMateREST/src/main/java/practice/askmaterest/services/AnswerService.java
@@ -1,9 +1,31 @@
 package practice.askmaterest.services;
 
-import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import practice.askmaterest.model.Answer;
+import practice.askmaterest.model.Post;
+import practice.askmaterest.services.daos.AnswerRepo;
+
+import java.util.List;
+
 
 @Service
-@AllArgsConstructor
 public class AnswerService {
+    private final AnswerRepo answerRepo;
+
+    private final int numberOfPreviewAnswers = 5;
+    private final Pageable previewPage = PageRequest.of(0,numberOfPreviewAnswers);
+
+    public AnswerService(AnswerRepo answerRepo) {
+        this.answerRepo = answerRepo;
+    }
+
+    public List<Answer> getAnswerPreviewToPost(Post post) {
+        return answerRepo.findAllByPostOrderById(post,previewPage);
+    }
+    public List<Answer> getAnswersToPost(Post post, int page) {
+        int numberOfAnswersPerPage = 10;
+        return answerRepo.findAllByPostOrderById(post,PageRequest.of(page, numberOfAnswersPerPage));
+    }
 }
--- AskMateREST/src/main/java/practice/askmaterest/services/daos/AnswerRepo.java
+++ AskMateREST/src/main/java/practice/askmaterest/services/daos/AnswerRepo.java
@@ -1,7 +1,12 @@
 package practice.askmaterest.services.daos;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import practice.askmaterest.model.Answer;
+import practice.askmaterest.model.Post;
+
+import java.util.List;
 
 public interface AnswerRepo extends JpaRepository<Answer, Long> {
+    List<Answer> findAllByPostOrderById(Post post, Pageable pageable);
 }
