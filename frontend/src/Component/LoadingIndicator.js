843afcc1a978f287c4f8a7536e6c99ce19230cfb
Z-SooS <zoli.soos98@gmail.com>
2022-11-15 11:04:24 +0100

    Create Loading Indicator

---
+++ frontend/src/Component/LoadingIndicator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ComponentStyle/LoadingIndicator.css';
+
+function LoadingIndicator() {
+    return (
+        <div className={"lds-dual-ring"}></div>
+    );
+}
+
+export default LoadingIndicator;
