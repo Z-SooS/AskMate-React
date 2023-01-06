26874fb448289b9bbec8ab5074694ff95abcb4cd
Z-SooS <zoli.soos98@gmail.com>
2023-01-06 10:15:02 +0100

    Create Home page element

---
+++ frontend/src/Page/HomePage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import CustomLink from "../Component/CustomLink";
+import "../ComponentStyle/HomePage.css"
+
+function HomePage({userInfo}) {
+    return (
+        <>
+            <section id={"home-page-title"}>
+                AskMatyi
+            </section>
+            <nav id={"home-page-options"}>
+                {userInfo != null ?
+                    <>
+                        <CustomLink to={"/posts"}>See posts</CustomLink>
+                        <CustomLink to={"/profile"}>Profile</CustomLink>
+                    </>
+                :
+                    <>
+                        <CustomLink to={"/register"}>Create new account</CustomLink>
+                        <CustomLink to={"/login"}>Already have an account</CustomLink>
+                    </>
+                }
+            </nav>
+        </>
+    );
+}
+
+export default HomePage;
