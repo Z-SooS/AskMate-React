a1ac53ee76474ffd72c021f04ed32de642731325
Z-SooS <zoli.soos98@gmail.com>
2023-01-06 10:11:08 +0100

    Create PageFooter component

---
+++ frontend/src/Component/PageFooter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../ComponentStyle/PageFooter.css';
+
+function PageFooter() {
+    return (
+        <footer id={"page-footer"}>
+            <span>All rights reserved. <a href={"https://github.com/Z-SooS/AskMate-React"}>FrendSzy</a></span>
+        </footer>
+    );
+}
+
+export default PageFooter;
