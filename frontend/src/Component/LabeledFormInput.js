ecfc9bed678036200b8ff1db9e18fb2bc91807d1
Z-SooS <zoli.soos98@gmail.com>
2023-01-06 10:17:07 +0100

    Create component for form inputs

---
+++ frontend/src/Component/LabeledFormInput.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import {fieldType} from "../Config/FormFieldData";
+
+function LabeledFormInput({name,label,inputType,checkerFunction}) {
+    return (
+        <label id={name+fieldType.label} htmlFor={name} className={"form-input-group"}>
+            {label}:
+            <input type={inputType} id={name+fieldType.input} className={"form-input-field"} name={name} onChange={checkerFunction}/>
+            <div id={name+fieldType.error} className={"form-input-error"}></div>
+        </label>
+    );
+}
+
+export default LabeledFormInput;
+
+LabeledFormInput.propTypes = {
+    name: PropTypes.string,
+    inputType: PropTypes.string,
+    label: PropTypes.string,
+    checkerFunction: PropTypes.func
+};
