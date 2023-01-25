aefd53fe9a22da32972d3d9c37b0f0a4ac153c21
Z-SooS <zoli.soos98@gmail.com>
2023-01-25 11:09:11 +0100

    Change environment variable name

--- frontend/src/Utility/APIRequests.js
+++ frontend/src/Utility/APIRequests.js
@@ -1,9 +1,9 @@
 const APIRequests = {
     get: async(route)=> {
-        return await fetch(`${process.env.BACKEND_ADDRESS}${route}`)
+        return await fetch(`${process.env.REACT_APP_BACKEND_ADDRESS}${route}`)
     },
     post: async(route, bodyObject)=> {
-        return await fetch(`${process.env.BACKEND_ADDRESS}${route}`, {
+        return await fetch(`${process.env.REACT_APP_BACKEND_ADDRESS}${route}`, {
             method:"POST",
             headers:{
                 'Content-Type': 'application/json'
