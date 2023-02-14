84e444cba457903e93988cd9d30a398a2837f1b2
Z-SooS <zoli.soos98@gmail.com>
2023-02-14 10:06:57 +0100

    Add breadcrumb element in App with setter functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CookieMethods from "./Config/CookieMethods";
 import cookieMethods from "./Config/CookieMethods";
 import PageFooter from "./Component/PageFooter";
 import HomePage from "./Page/HomePage";
+import BreadCrumbs from "./Component/BreadCrumbComponents/BreadCrumbs";
 
 function App() {
     function checkCookieForUsername(){
@@ -18,11 +19,24 @@ function App() {
         }
         return cookieMethods.parseJwt(headerPayloadValue);
     }
+    let breadCrumbSetFunc;
+    let breadCrumbAddFunc;
+    function passDownBreadCrumbAddFunc(name,value){
+        breadCrumbAddFunc(name,value);
+    }
+    function passDownBreadCrumbSetFunc(values = new Map([['Home','home']])){
+        breadCrumbSetFunc(values);
+    }
+    function setUpBreadCrumbs(gotResetFunction,gotAddFunction){
+        breadCrumbSetFunc = gotResetFunction;
+        breadCrumbAddFunc = gotAddFunction;
+    }
     const [userInfo, setUserInfo] = useState(checkCookieForUsername());
 
     return (
         <>
                 <Navbar userInfo={userInfo} userInfoSetter={setUserInfo}/>
+            <BreadCrumbs breadCrumbSetUp={setUpBreadCrumbs}/>
             <main>
                 <Routes>
                     <Route path={"/"} element={<HomePage userInfo={userInfo}/>}/>
