0d42993a332cb0614fc1ce60551bc468e9135911
Z-SooS <zoli.soos98@gmail.com>
2023-02-16 12:06:56 +0100

    Implement breadcrumbs in PostsPage.js, improve stability

--- frontend/src/Page/PostsPage.js
+++ frontend/src/Page/PostsPage.js
@@ -1,22 +1,35 @@
 import React, {useEffect, useState} from 'react';
 import LoadingIndicator from "../Component/LoadingIndicator";
-import PostContainer from "../Component/PostContainer";
 import AddPostModal from "../Component/AddPostModal";
 import APIRequests from "../Utility/APIRequests";
+import PostsPageItem from "../Component/PostList/PostsPageItem";
+import {useLocation, useParams} from "react-router-dom";
+import breadCrumbFunctions from "../Utility/BreadCrumbFunctions";
+import "../Utility/TagArray.js"
 
-function PostsPage(props) {
+function PostsPage() {
+    const TagArray = require("../Utility/TagArray")
+    const {pageFromPath} = useParams();
     let [data, setData] = useState([]);
     let [orderBy, setOrderBy] = useState("score");
     let [orderDir, setOrderDir] = useState("DESC");
-    let [page,setPage] = useState(0);
+    const [tags, setTags] = useState(new TagArray([]));
+    let [page,setPage] = useState(pageFromPath != null ? parseInt(pageFromPath) : 0);
+    let [postIsOpen, setPostIsOpen] = useState(false);
     const [error,setError] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    const location = useLocation();
+
     async function getData(){
-        await APIRequests.get(`/post-service/posts/${page}?order=${orderBy}&direction=${orderDir}`)
+        console.log('getData',tags);
+        await APIRequests.get(`/post-service/posts/${page}?order=${orderBy}&direction=${orderDir}${tags.array.map(t => "&tag="+t).join()}`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error(`The returned response was ${response.status}`);
+                }
+                if(response.status === 204) {
+                    throw new Error('No posts were found')
                 }
                 return response.json()
             })
@@ -30,19 +43,70 @@ function PostsPage(props) {
             })
             .finally(() => {setLoading(false);})
     }
+
     async function reloadFunction(){
         await getData();
     }
+
+    function setBreadCrumb(){
+        if(location.pathname === '/posts' && location.search === '') {
+            breadCrumbFunctions.set(new Map([
+                ['Home','home'],
+                ['Posts','posts']
+            ]));
+            setPage(0);
+            console.log('breadCrumb',page)
+            return;
+        }
+        breadCrumbFunctions.set(new Map([
+            ['Home','home'],
+            ['Posts','posts'],
+            ['Search',`posts/0?order=${orderBy}&direction=${orderDir}${tags.array.map(t => "&tag="+t).join()}`],
+            [`Page ${page+1}`,`posts/${page}?order=${orderBy}&direction=${orderDir}${tags.array.map(t => "&tag="+t).join()}`]
+        ]))
+    }
+
+    function setQueryParams() {
+        console.log('query params', page)
+        const queryParams = new URLSearchParams(location.search);
+        const queryTags = new TagArray(queryParams.getAll('tag'));
+        if (!queryTags.equals(tags)) {
+            setTags(queryTags);
+        }
+        const order = queryParams.get('order');
+        const direction = queryParams.get('direction');
+
+        if (order != null && order !== orderBy) setOrderBy(order);
+        if (direction != null && direction !== orderDir) setOrderDir(direction);
+        console.log('queryParam page', pageFromPath ,page)
+        if (pageFromPath != null) {
+            setPage(parseInt(pageFromPath));
+        } else {
+            setPage(0);
+        }
+    }
     useEffect(() => {
+        console.log('effect tags',tags)
+        console.log('effect order',orderBy)
+        console.log('effect dir',orderDir)
+        console.log('effect page',page)
+        setBreadCrumb();
         setLoading(true);
         getData();
-    }, [page,orderDir,orderBy]);
+    }, [page,orderDir,orderBy,tags]);
+    useEffect(() => {
+        console.log('location change',location)
+        setPostIsOpen(false);
+        setQueryParams();
+    }, [location.pathname,location.search]);
     if(error) return (<div className={"error"} id={"response-error-box"}>{`There is a problem fetching data from the server - ${error}`}</div>);
     return (
         <>
+            {/*Debug*/}
+            {/*<Link to={`/posts/${page+1}?order=${orderBy}&direction=${orderDir}${tags.array.map(t => "&tag="+t).join()}`} onClick={()=>{setPage(page+1)}}>Increase page number</Link>*/}
             <AddPostModal reloadFunction={reloadFunction}/>
             {loading && <LoadingIndicator/>}
-            {data.length > 0 && data.map(p => (<PostContainer postObject={p} key={p.post.id}></PostContainer>))}
+            {data.length > 0 && data.map(p => (<PostsPageItem postObject={p} key={p.id}></PostsPageItem>))}
         </>
     );
 }
