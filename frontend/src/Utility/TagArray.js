5af80e3b5e85f23d646c23fc551deeb5e8549a1d
Z-SooS <zoli.soos98@gmail.com>
2023-02-16 11:42:01 +0100

    Create prototype to compare tag arrays

---
+++ frontend/src/Utility/TagArray.js
@@ -0,0 +1,27 @@
+function TagArray(arrayOfIds){
+    this.array = arrayOfIds;
+}
+
+TagArray.prototype.equals = function(comparedTagArray){
+    if(!comparedTagArray.array) return false;
+    if(comparedTagArray === this) return true;
+    if(comparedTagArray.array.length !== this.array.length) return false;
+
+    const copyArray = new TagArray(comparedTagArray.array);
+
+    for(let i=0; i < this.array.length; i++){
+        const index = copyArray.tagIndexOf(this.array[i]);
+        if(index !== -1) copyArray.array.splice(index, 1);
+    }
+    return copyArray.array.length <= 0;
+
+}
+
+TagArray.prototype.tagIndexOf = function (tagIdToFind) {
+    for (let i=0; i<this.array.length; i++) {
+        if(tagIdToFind === this.array[i]) return i;
+    }
+    return -1;
+}
+
+module.exports = TagArray;
