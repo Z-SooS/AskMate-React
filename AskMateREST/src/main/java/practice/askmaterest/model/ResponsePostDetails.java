b607d554782191a7812a9e63e381cacb9c2455dd
Z-SooS <zoli.soos98@gmail.com>
2023-02-16 11:38:14 +0100

    Change ResponsePostDetails to include the more details, add set of "Answer"s to"Post" model

--- AskMateREST/src/main/java/practice/askmaterest/model/ResponsePostDetails.java
+++ AskMateREST/src/main/java/practice/askmaterest/model/ResponsePostDetails.java
@@ -1,13 +1,31 @@
 package practice.askmaterest.model;
 
-import lombok.Builder;
 import lombok.Getter;
 
-import java.util.List;
+import java.sql.Timestamp;
+import java.util.Set;
 
 @Getter
-@Builder
 public class ResponsePostDetails {
-    private Post post;
-    private List<Answer> answers;
+    private Long id;
+    private WebUser user;
+    private Timestamp dateCreated;
+    private Set<Tag> tags;
+    private String title;
+    private String message;
+    private int numberOfAnswers;
+    private int score;
+
+    public static ResponsePostDetails ConvertFromPost(Post post) {
+        var result = new ResponsePostDetails();
+        result.id = post.getId();
+        result.user = post.getUser();
+        result.dateCreated  = post.getDateCreated();
+        result.score = post.getScore();
+        result.title = post.getTitle();
+        result.message = post.getMessage();
+        result.tags = post.getTags();
+        result.numberOfAnswers = post.getAnswers().size();
+        return result;
+    }
 }
