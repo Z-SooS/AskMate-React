7af7a700b7b7bac8f5493bd8f3aff9783f707188
Z-SooS <zoli.soos98@gmail.com>
2023-02-18 11:52:25 +0100

    Fix copy creation for 'equals' function. Add optional return to loop in 'equals'. Add toString function to TagArray.js

--- frontend/src/Utility/TagArray.js
+++ frontend/src/Utility/TagArray.js
@@ -7,16 +7,20 @@ TagArray.prototype.equals = function(comparedTagArray){
     if(comparedTagArray === this) return true;
     if(comparedTagArray.array.length !== this.array.length) return false;
 
-    const copyArray = new TagArray(comparedTagArray.array);
+
+    const copyArray = new TagArray([...comparedTagArray.array]);
 
     for(let i=0; i < this.array.length; i++){
         const index = copyArray.tagIndexOf(this.array[i]);
-        if(index !== -1) copyArray.array.splice(index, 1);
+        if(index === -1) return false;
+        copyArray.array.splice(index, 1)
     }
     return copyArray.array.length <= 0;
 
 }
-
+TagArray.prototype.toString = function(){
+    return `[${this.array.join(', ')}]`
+}
 TagArray.prototype.tagIndexOf = function (tagIdToFind) {
     for (let i=0; i<this.array.length; i++) {
         if(tagIdToFind === this.array[i]) return i;
